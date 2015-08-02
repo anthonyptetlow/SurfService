34d25454b9f536bb93e6302ab4d424a964a76907
anthonyptetlow <anthonyptetlow@gmail.com>
2015-08-02 12:07:27 +0100

    Removed comments and removed invalid postinstall script

--- index.js
+++ index.js
@@ -28,17 +28,10 @@ app.use(session({
 
 
 app.use(function(req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', '*');
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
-
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-
-
-// app.use(express.static(__dirname + '/../public'));
 
 //Enable Route Logging
 app.use('/api', morgan('dev'));
