ce5014a7483894820553c1b5cce04a34c298c47f
anthonyptetlow <anthonyptetlow@gmail.com>
2015-09-20 10:33:35 +0100

    Removed unused route and tweaked the Locations model

--- Surf/models/Location.js
+++ Surf/models/Location.js
@@ -6,15 +6,29 @@ module.exports = mongoose.model('Location',
 		name: String,
 		bearchPerp: Number,
 		bayImagePath: String,
-		image: String,
+		image: {
+			url: String,
+			width: Number,
+			height: Number
+		},
 		location: {
 			type: [Number],
 			index: '2dsphere',
 			defualt: undefined
+		},
+		region : {
+			type: Schema.Types.ObjectId,
+			ref: 'Region'
 		}
 	})
 );
+// Europe UK + Ireland Scotland Inner Hebrides Machrihanish
+// Continent, Country, Region , Subregion
 
+// {
+// 	regionName:
+// 	parentRegion:
+// }
 // schema.pre('save', function (next) {
 //   if (this.isNew && Array.isArray(this.location) && 0 === this.location.length) {
 //     this.location = undefined;
